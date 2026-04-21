9453423f4708a67b09f08e18080e6ea6963b8cae
energymaster22 <energymastervr@gmail.com>
2026-04-21 02:20:46 -0400

    actually finish up items.py

--- worlds/ios_shortcuts/items.py
+++ worlds/ios_shortcuts/items.py
@@ -15,3 +15,21 @@ class IosShortcutsItem(Item):
 
 def get_random_filler_item_name(world: IosShortcutsWorld) -> str:
     return "Filler"
+
+def create_item_with_correct_classification(world: IosShortcutsWorld, name: str) -> IosShortcutsItem:
+    classification = DEFAULT_ITEM_CLASSIFICATIONS[name]
+
+def create_all_items(world: IosShortcutsWorld) -> None:
+    itempool: list[Item] = [
+        world.create_item("Shortcut Activation")
+    ]
+
+    number_of_items = len(itempool)
+
+    number_of_unfilled_locations = len(world.multiworld.get_unfilled_locations(world.player))
+
+    needed_number_of_filler_items = number_of_unfilled_locations - number_of_items
+
+    itempool += [world.create_filler() for _ in range(needed_number_of_filler_items)]
+
+    world.multiworld.itempool += itempool
